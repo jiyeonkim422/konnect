d20518b3d399d818a3b2b6ce0e56b2d74d36d642
jiyeon kim <jiyeon.kim@konnect.finance>
2023-09-08 18:32:49 +0900

    앱 메인화면 업데이트

--- konnect_v1.2/js/common.js
+++ konnect_v1.2/js/common.js
@@ -2,27 +2,27 @@
 //slider-notice.js
 
 $(document).ready(function(){
-	var height =  $(".notice").height(); //공지사항의 높이값을 구해주고~~
-	var num = $(".rolling li").length; // 공지사항의 개수를 알아볼수 있어요! length라는 것으로!
-	var max = height * num; //그렇다면 총 높이를 알 수 있겠죠 ?
-	var move = 0; //초기값을 설정해줍니다.
+	var height =  $(".notice").height(); 
+	var num = $(".rolling li").length; 
+	var max = height * num;
+	var move = 0; 
 	function noticeRolling(){
-		move += height; //여기에서 += 이라는 것은 move = move + height 값이라는 뜻을 줄인 거에요.
-		$(".rolling").animate({"top":-move},600,function(){ // animate를 통해서 부드럽게 top값을 올려줄거에요.
-			if( move >= max ){ //if문을 통해 최대값보다 top값을 많이 올렸다면 다시 !
-				$(this).css("top",0); //0으로 돌려주고~
-				move = 0; //초기값도 다시 0으로!
+		move += height; 
+		$(".rolling").animate({"top":-move},600,function(){ 
+			if( move >= max ){ 
+				$(this).css("top",0); 
+				move = 0; 
 			};
 		});
 	};
-	noticeRollingOff = setInterval(noticeRolling,3000); //자동롤링답게 setInterval를 사용해서 1000 = 1초마다 함수 실행!!
-	$(".rolling").append($(".rolling li").first().clone()); //올리다보면 마지막이 안보여서 clone을 통해 첫번째li 복사!
+	noticeRollingOff = setInterval(noticeRolling,3000); 
+	$(".rolling").append($(".rolling li").first().clone());
 
 	$(".rolling_stop").click(function(){
-		clearInterval(noticeRollingOff); //stop을 누르면 clearInterval을 통해 자동롤링을 해제합니다.
+		clearInterval(noticeRollingOff); 
 	});
 	$(".rolling_start").click(function(){
-		noticeRollingOff = setInterval(noticeRolling,1000); //다시 start를 누르면 자동롤링이 실행하도록 !!
+		noticeRollingOff = setInterval(noticeRolling,1000); 
 	});
 });
 
@@ -70,8 +70,9 @@ var swiper = new Swiper(".my_membership_swiper", {
 
 //메인 - 혜택 슬라이드
   var swiper = new Swiper(".benefit_swiper", {
-    spaceBetween: 0,
-    loop:true,
+    slidesPerView: "auto",
+    spaceBetween: 30,
+    centeredSlides: true,
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
@@ -80,6 +81,8 @@ var swiper = new Swiper(".my_membership_swiper", {
 
   //메인 -  이벤트 배너 슬라이드
   var swiper = new Swiper(".event_banner_swiper", {
+    slidesPerView: "auto",
+    spaceBetween: 30,
     loop:true,
 	    pagination: {
         el: ".swiper-pagination",
@@ -89,8 +92,9 @@ var swiper = new Swiper(".my_membership_swiper", {
 
   //리뷰 슬라이드
   var swiper = new Swiper(".review_swiper", {
+    slidesPerView: "auto",
     spaceBetween: 30,
-    loop:true,
+    centeredSlides: true,
   });
 
   //브랜드 상세 - 이미지 풀사이즈 슬라이드
