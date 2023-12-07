987e905d6b11d98f31d97569a9803a850b86aa73
jiyeon kim <jiyeon.kim@konnect.finance>
2023-12-07 18:08:01 +0900

    커넥트월드 2.0 업데이트

--- konnect_world_v.2.0/js/common.js
+++ konnect_world_v.2.0/js/common.js
@@ -1,31 +1,3 @@
-$('.center').slick({
-    centerMode: true,
-    centerPadding: '60px',
-    slidesToShow: 3,
-    responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 3
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 1
-        }
-      }
-    ]
-  });
-          
-
-
 // more_btn
 $(function () {
     $('.tabcontent > div').hide();
@@ -42,4 +14,35 @@ $(function () {
         .filter(':eq(0)')
         .click();
 });
+
+
+  
+
+gsap.registerPlugin(ScrollTrigger);
+
+  ScrollTrigger.create({
+  trigger: ".content_2", // 감시할 요소
+  start: "80% center",   // 감시가 시작될 위치
+  end: "100% center",  // 감시가 종료될 위치
+  markers: false,         // 디버깅을 위한 마커 표시
+  scub: 3,
+  onEnter: () => {
+    // 스크롤이 해당 영역에 들어왔을 때의 동작
+    gsap.to(".content_2", {
+      borderRadius:"20px",
+      width: "calc(100% - 8%)", 
+      ease: "power2.out",
+    });        
+  },
+  onLeaveBack: () => {
+    // 스크롤이 해당 영역을 빠져나갈 때의  동작
+    gsap.to(".content_2", {
+      borderRadius: "0", // 다시 사각형 모양으로 설정
+      width:'100%',
+      ease: "power2.inOut",
+    });
+  },
+});
+
+
 
